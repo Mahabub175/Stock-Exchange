feac959bab99c462b00123ab4eb70f3e500a40b6
Mahabub175 <rgrxmahabub.official@gmail.com>
2025-04-15 15:42:44 +0600

    Updated

--- src/assets/data/sliderData.js
+++ src/assets/data/sliderData.js
@@ -1,11 +1,11 @@
-import crypto1 from "@/assets/images/crypto1.png";
-import crypto2 from "@/assets/images/crypto2.png";
-import crypto3 from "@/assets/images/crypto3.png";
-import crypto4 from "@/assets/images/crypto4.png";
-import crypto5 from "@/assets/images/crypto5.png";
-import crypto6 from "@/assets/images/crypto6.png";
-import crypto7 from "@/assets/images/crypto7.png";
-import crypto8 from "@/assets/images/crypto8.png";
+import slider1 from "@/assets/images/slider1.png";
+import slider2 from "@/assets/images/slider2.png";
+import slider3 from "@/assets/images/slider3.png";
+import slider4 from "@/assets/images/slider4.png";
+import slider5 from "@/assets/images/slider5.png";
+import slider6 from "@/assets/images/slider6.png";
+import slider7 from "@/assets/images/slider7.png";
+import slider8 from "@/assets/images/slider8.png";
 
 import btc from "@/assets/images/btc.png";
 import eth from "@/assets/images/eth.png";
@@ -20,14 +20,14 @@ import tesla from "@/assets/images/tesla.png";
 import amazon from "@/assets/images/amazon.png";
 
 export const cryptoData = [
-  { id: 1, image: crypto1, icon: apple },
-  { id: 2, image: crypto2, icon: tesla },
-  { id: 3, image: crypto3, icon: google },
-  { id: 4, image: crypto4, icon: amazon },
-  { id: 5, image: crypto5, icon: microsoft },
-  { id: 6, image: crypto6, icon: btc },
-  { id: 7, image: crypto7, icon: eth },
-  { id: 8, image: crypto8, icon: dodge },
-  { id: 9, image: crypto3, icon: bnb },
-  { id: 10, image: crypto6, icon: sol },
+  { id: 1, image: slider1, icon: apple },
+  { id: 2, image: slider2, icon: tesla },
+  { id: 3, image: slider3, icon: google },
+  { id: 4, image: slider4, icon: amazon },
+  { id: 5, image: slider5, icon: microsoft },
+  { id: 6, image: slider6, icon: btc },
+  { id: 7, image: slider7, icon: eth },
+  { id: 8, image: slider8, icon: dodge },
+  { id: 9, image: slider3, icon: bnb },
+  { id: 10, image: slider6, icon: sol },
 ];
